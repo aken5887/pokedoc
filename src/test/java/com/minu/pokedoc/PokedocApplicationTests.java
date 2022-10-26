dd7d934e8a81955851d4e7be50407b457e102f46
aken <aken@kakao.com>
2022-10-26 14:30:38 +0900

    Initial commit

---
+++ src/test/java/com/minu/pokedoc/PokedocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minu.pokedoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedocApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
