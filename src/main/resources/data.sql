ee14aedc0d3ab1797423f1260865ba501d2b6b2f
aken <aken@kakao.com>
2022-11-08 21:10:41 +0900

    real profile 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-insert into tb_category (name, desc) values ('halloween', '할로윈 한정 띠뿌실');
-insert into tb_category (name, desc) values ('normal', '일반 띠뿌실')
+insert into tb_category (name, category_desc) values ('halloween', '할로윈 한정 띠뿌실');
+insert into tb_category (name, category_desc) values ('normal', '일반 띠뿌실')
