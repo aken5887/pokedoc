70f105a059adc01cb1f161da215a8eb2ad4cfb99
aken <aken@kakao.com>
2022-11-09 16:37:44 +0900

    snapshot 버전 및 날짜 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -23,12 +23,12 @@ fi
 
 echo ">>>>>>> 새 애플리케이션 배포"
 
-JAR_NAME=$(ls -tr $REPOSITORY/ | grep SNAPSHOT.jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo ">>>>>>> JAR_NAME : $REPOSITORY/$JAR_NAME"
 echo ">>>>>>> $JAR_NAME 에 실행권한 추가"
 
-chmod +x $JAR_NAME
+chmod +x $REPOSITORY/$JAR_NAME
 
 echo ">>>>>>> $JAR_NAME 실행"
 
