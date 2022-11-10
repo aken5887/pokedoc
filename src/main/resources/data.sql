503357ba15b4fe61c9fceaf7d29d99f7cd6ad7e1
aken <aken@kakao.com>
2022-11-10 18:36:18 +0900

    중복페이지 만듬

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
-delete from tb_category;
 insert into tb_category (name, category_desc) values ('halloween', '할로윈');
+insert into tb_category (name, category_desc) values ('halloween2nd', '할로윈-중복');
 insert into tb_category (name, category_desc) values ('general', '1세대 & 2세대');
+insert into tb_category (name, category_desc) values ('general2nd', '1세대 & 2세대-중복');
