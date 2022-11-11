6c9ca8ef9d4a14c5b98e6d2073654cc6ca1e45b5
aken <aken@kakao.com>
2022-11-11 15:40:17 +0900

    둘러보기 페이지 사용자 이름 수정

--- src/main/java/com/minu/pokedoc/web/dto/User/UserDexDto.java
+++ src/main/java/com/minu/pokedoc/web/dto/User/UserDexDto.java
@@ -3,7 +3,9 @@ package com.minu.pokedoc.web.dto.User;
 import com.minu.pokedoc.domain.user.User;
 import java.util.List;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @Getter
 public class UserDexDto {
   private Long id;
@@ -13,11 +15,35 @@ public class UserDexDto {
 
   public UserDexDto(User user) {
     this.id = user.getId();
-    this.name = user.getName();
+    this.name = convertUserName(user.getName());
     this.picture = user.getPicture();
   }
 
   public void update(List<UserStickerDexDto> stickers){
     this.stickers = stickers;
+  }
+
+  private String convertUserName(String userName){
+    String newName = "";
+    switch(userName.length()){
+      case 2:
+      case 3:
+        newName = userName.replace(userName.charAt(1), '*');
+        break;
+      case 4:
+        newName = userName.replace(userName.charAt(1), '*')
+            .replace(userName.charAt(2), '*');
+        break;
+      default:
+        if(userName.length()>4){
+          newName = userName.replace(userName.charAt(1), '*')
+              .replace(userName.charAt(2), '*')
+              .replace(userName.charAt(3), '*');
+        }else{
+          newName = userName;
+        }
+        break;
+    }
+    return newName;
   }
 }
